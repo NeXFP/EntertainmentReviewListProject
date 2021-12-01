31ecc5820df3bf4f0ef3c1be058446718a7bdfaa
NeXFP <giovannycardoso50@gmail.com>
2021-12-01 09:10:50 -0500

    Added post.hbs and changed port to heroku

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const path = require('path');
 const express = require('express');
-const router = express.Router();
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 80;
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -21,13 +20,8 @@ const sess = {
 };
 
 app.use(session(sess));
-/*
-This can be removed becauase I reinstalled the correct version of exphbs(version 3): 
-const helpers = require('./utils/helpers');
 
-const hbs = exphbs.create({ helpers });
-*/
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
@@ -39,5 +33,3 @@ app.use(require('./controllers'));
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`Now listening on ${PORT}`))
 });
-
-module.exports = router;
