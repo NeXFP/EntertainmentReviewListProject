84ceb97566784c704294ffcb9ee20f3c3aedd883
Adam <a.6942@yahoo.com>
2021-12-01 20:51:44 -0500

    updated Music model

---
+++ models/Music.js
@@ -0,0 +1,29 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Music extends Model {}
+
+Music.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        category_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'category',
+    }
+);
+
+module.exports = Music;
