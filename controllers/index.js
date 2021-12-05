ba28e45f8d9851c91991cd42a8f55a51c74e9b94
Adam <a.6942@yahoo.com>
2021-12-05 14:18:03 -0500

    reverted change

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,9 @@
 const router = require('express').Router();
 const apiRoutes  = require('./api');
 const htmlRoutes = require('./html-routes.js')
-const loginRoutes = require('./login-route')
 
 router.use('/api', apiRoutes);
 router.use('/', htmlRoutes);
-router.use('/login', loginRoutes)
 
 router.use("*",(req, res) => {
     res.send("<h1>You're going the wrong way! </h1>")
